c721ad45a20e43213eb726b2960196db4d038688
Yves Raimond <moustaki@lorie.lan>
2009-07-13 14:11:51 +0100

    Small bot run script

---
+++ run_bots.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./irc_bots/connectionfinder.py irc.freenode.net "#bbcmusicbore" connectionfinder
+./irc_bots/placefinder.py irc.freenode.net "#bbcmusicbore" placefinder
+./irc_bots/trackfinder.py irc.freenode.net "#bbcmusicbore" trackfinder
+./irc_bots/hotnessfinder.py irc.freenode.net "#bbcmusicbore" hotnessfinder
